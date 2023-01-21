b6ed850a0d6fe00fead6bf3b5681b825238e6422
Auriatta <Auriattadev@protonmail.com>
2023-01-22 00:51:09 +0100

    Replacing search method to regex
    aproaching with new methodology for searching through a website html code. Replacing old custom methods to regex functions. Writing down the missing parts as a comments

--- main.py
+++ main.py
@@ -17,38 +17,33 @@ from keep_alive import keep_alive
 
 sims4WebSite = website_maintance.html_raw_formater()
 url = 'https://www.ea.com/games/the-sims/the-sims-4/news/update-07-26-2022'
+
+#rand from valid urls
+
 siteContent = sims4WebSite.getWebSiteContent('https://www.ea.com/games/the-sims/the-sims-4/news/update-07-26-2022')
 #siteContent = sims4WebSite.getWebSiteContent('https://www.ea.com/games/the-sims/the-sims-4/news/update-08-02-2022')
 
 siteContent = re.sub('\n','', siteContent)
 siteContent = re.sub('\r','', siteContent)
+siteContent = re.sub('\t','', siteContent)
 
 print('Site target url: ', url)
 print('Content site size: ', len(siteContent), ' bytes')
 
 
-siteContent = sims4WebSite.getWebSiteSpecificPart('Bug Fixes', '<', 'Related News', '<', siteContent)
+siteContent = re.search(r"Bug Fixes<.(.*)</div>", siteContent).group(1)
 
-#cut all <ul (...)> to <ul>
+siteContent = re.sub(r"<ul.*?>", "<ul>", siteContent)
 
-siteContentListsGroups = []
-while 1 :
-    siteContentListsGroups.append(sims4WebSite.getWebSiteSpecificPart'<ul><li', '>', '</li></ul', '>', siteContent))
-    siteContentListsGroupsSize = len(siteContentListsGroups)-1
-    siteContent = re.sub(siteContentListsGroups[siteContentListsGroupsSize],'', siteContent)
-    if siteContentListsGroups[siteContentListsGroupsSize] == siteContent:
-        break
-
+siteContentListsGroups = re.findall(r"<ul><li>.*?</li></ul>",siteContent)
 print('Content site size reduced to: ', len(siteContent), ' bytes')
 
-siteContent = siteContent.split('</li>')
-siteContent =  sims4WebSite.formatContentList(siteContent)
-
-
-
+# rand from siteContentListsGroups
+# split into list <li></li>
+# rand from list and print
 
 print('Content view:')
-print(siteContent )
+print(siteContentListsGroups )
 
 
 #predef.client.run(predef.os.environ['TOKEN'])
